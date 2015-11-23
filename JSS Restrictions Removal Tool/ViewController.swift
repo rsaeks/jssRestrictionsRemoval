0f6982eda41627615874824ff7810a78c090f040
rsaeks <rsaeks@gmail.com>
2015-11-23 10:21:15 -0600

    Initial commit from local git

---
+++ JSS Restrictions Removal Tool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSS Restrictions Removal Tool
+//
+//  Created by Randy Saeks on 11/23/15.
+//  Copyright © 2015 Randy Saeks. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
