926e4507445a6aa5ec3b930cac42de041148577f
rsaeks <rsaeks@gmail.com>
2015-11-23 21:36:11 -0600

    Status indicators
    Added status indicators

---
+++ Carthage/Checkouts/Just/Just.podspec
@@ -0,0 +1,43 @@
+
+Pod::Spec.new do |s|
+
+  s.name         = "Just"
+  s.version      = "0.4.4"
+  s.summary      = "Swift HTTP for Humans"
+
+  s.description  = <<-DESC
+                   Just is a HTTP library with an elegant interface inspired by (python-request)[http://python-request.org]
+
+                   Features includes:
+
+                   -    URL queries
+                   -    custom headers
+                   -    form (x-www-form-encoded) / JSON HTTP body
+                   -    redirect control
+                   -    multpart file upload along with form values.
+                   -    basic/digest authentication
+                   -    cookies
+                   -    timeouts
+                   -    synchrounous / asyncrounous requests
+                   -    upload / download progress tracking for asynchronous requests
+                   -    link headers
+                   -    friendly accessible results
+                   DESC
+
+  s.homepage     = "http://justhttp.net"
+
+  s.license      = { :type => "MIT", :file => "LICENSE.md" }
+  s.author             = { "Daniel Duan" => "daniel@duan.org" }
+  s.social_media_url   = "https://twitter.com/JustHTTP"
+
+   s.ios.deployment_target = "8.0"
+   s.osx.deployment_target = "10.10"
+
+  s.source       = {
+    :git => "https://github.com/JustHTTP/Just.git",
+    :tag => "v#{s.version}"
+  }
+
+  s.source_files  = "Just", "Just/**/*.{swift}"
+
+end
--- JSS Restrictions Removal Tool/ViewController.swift
+++ JSS Restrictions Removal Tool/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import Cocoa
+import Just
+
+
 
 class ViewController: NSViewController {
 
@@ -14,6 +17,46 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // DEBUG: Console Output for loading the app
+        print("View did Load")
+    }
+    // Create Connection to JSS URL from Text Box
+    @IBOutlet weak var jssURL: NSTextField!
+    
+    //Initialize Connection to JSS Status indicator gems
+    @IBOutlet weak var jssConnectYes: NSImageView!
+    @IBOutlet weak var jssConnectNo: NSImageView!
+    @IBOutlet weak var jssConnectTBD: NSImageView!
+    
+    @IBAction func checkJSSURL(sender: NSButton) {
+        // Reset gems
+        jssConnectTBD.hidden = false
+        jssConnectYes.hidden = true
+        jssConnectNo.hidden = true
+        
+        //DEBUG: Console output to button pressed
+        print ("Button Pressed")
+        
+        //DEBUG: Console output of URL of JSS
+        print (jssURL.stringValue)
+        
+        // Get URL of site from server
+        let r = Just.get(jssURL.stringValue)
+        
+        //DEBUG: Print the data returned by server
+        print(r.url)
+        
+        // As long as we get data back from server set status gem to green and hide red / grey
+        if (r.url != nil ) {
+            jssConnectTBD.hidden = true
+            jssConnectYes.hidden = false
+        }
+            // If we get back nil set status gem to red and hide green and grey
+        else {
+            jssConnectTBD.hidden = true
+            jssConnectNo.hidden = false
+        }
+        
     }
 
     override var representedObject: AnyObject? {
